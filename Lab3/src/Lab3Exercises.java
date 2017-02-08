e60e255e87e3337d14376d1d64494ef74c8e7932
4015e8 <4015e8@parcha.ece.uprm.edu>
2017-02-08 18:09:15 -0400

    I Changed the JFrame title to my name, and the JFrame size to 200 x 200. Also, adjusted the location so that the window shows up somewhat approximately in the center of the display when the program is run.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,23 @@
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		JFrame myFrame = new JFrame("Lysa");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		 GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	        int width = gd.getDisplayMode().getWidth();
+	        int height = gd.getDisplayMode().getHeight();
+	        int JFrameWidth = 200;
+	        int JFrameHeight = 200;
+	        myFrame.setLocation((width/2)-JFrameWidth/2, (height/2)-JFrameHeight/2);
+	        myFrame.setSize(JFrameWidth, JFrameHeight);
+
+
 	}
 }
